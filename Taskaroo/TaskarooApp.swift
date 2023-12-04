5ba6fd021f85313df0c156449b7ec045e212f8e2
Karolina Groszewska <karolina.groszewska@gmail.com>
2023-12-04 17:11:39 -0600

    Initial Commit

---
+++ Taskaroo/TaskarooApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskarooApp.swift
+//  Taskaroo
+//
+//  Created by Kari Groszewska on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskarooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
