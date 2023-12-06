e0a2aeecc2d878fc79c4c8f31f43f7c142a4f057
Karolina Groszewska <karolina.groszewska@gmail.com>
2023-12-05 19:53:28 -0600

    part 2

---
+++ Taskaroo/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  Taskaroo
+//
+//  Created by Kari Groszewska on 12/5/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Task{
+    var id: Int
+    var name: String
+    var description: String
+    var isCompleted: Bool
+    var timeTracker: [Date]
+}
---
+++ Taskaroo/TaskRow.swift
@@ -0,0 +1,18 @@
+//
+//  TaskRow.swift
+//  Taskaroo
+//
+//  Created by Kari Groszewska on 12/5/23.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskRow()
+}
